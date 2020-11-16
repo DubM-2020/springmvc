257b28037c15cc0d3164be6836a1b10b5fe45fc4
MM <imyeine.0@gmail.com>
2020-11-16 18:36:17 +0900

    SPRING(MVC_PATTERN) MVP PROJECT 업로드

---
+++ SQL/MVP_AUDIO.sql
@@ -0,0 +1,604 @@
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (1, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (2, ' 산들', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (3, ' 제시', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (4, ' 화사', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (5, ' 싹쓰리', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (6, ' 박진영', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (7, ' 스탠딩 에그', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (8, ' BLACKPINK', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (10, ' 규현 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (11, ' 오마이', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (12, ' 조정석', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (13, ' 블루 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (14, ' 이하이', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (15, ' 오반', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (16, ' 지코 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (18, ' (여자)아이들', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (19, ' Tones', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (20, ' ITZY ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (21, ' Maroon ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (22, ' 오마이걸', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (23, ' 장범준', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (24, ' 창모 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (25, ' AKMU ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (26, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (27, ' 전상근', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (28, ' 전미도', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (29, ' BLACKPI', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (30, ' 노을 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (31, ' 싹쓰리', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (32, ' 백지영', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (33, ' 선미', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (34, ' 폴킴', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (35, ' Dua Lipa', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (36, ' Anne Marie', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (37, ' 마크툽', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (38, ' 엠씨더맥스', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (39, ' 지코 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (40, ' 아이유', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (41, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (42, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (43, ' 신예영', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (44, ' 가호 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (45, ' 아이유', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (46, ' 비룡', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (47, ' 폴킴', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (48, ' 싹쓰리', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (49, ' Conan Gray', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (50, ' 조이 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (51, ' 10CM', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (52, ' Etham', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (53, '레드벨벳', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (54, ' 아이유', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (55, ' 오반', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (56, ' 임영웅', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (57, ' 볼빨간사춘기', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (58, ' 린다G', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (59, ' Lauv', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (60, ' 황인욱', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (61, ' 김필', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (62, ' TWICE', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (63, ' 청하', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (64, ' Billie ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (65, ' 유아 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (66, ' 규현 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (67, ' 청하', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (68, ' 카더가든', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (69, ' Ruel', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (70, ' 백현 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (71, ' Ariana Grande', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (72, ' 신용재', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (73, ' 폴킴', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (74, ' 방탄소', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (75, ' 런치', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (76, ' 진민호', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (77, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (78, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (79, ' Apink', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (80, ' 마크툽', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (81, ' Sam Smith', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (82, ' 마마무', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (83, ' IZ_ONE ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (84, ' 백현 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (85, ' 전상근', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (86, ' HYNN ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (87, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (88, ' 레드벨벳', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (89, ' 하현우', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (90, ' 임영웅', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (91, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (92, ' 폴킴', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (93, ' 미도와 파라솔', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (94, ' 백현 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (95, ' ITZY ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (96, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (97, ' 휘인 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (98, ' 어반자카파', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (99, ' 방탄소년단', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (100, ' 태연 ', null);
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (101, '방탄소년단', '<br>널 위해서라면 난
+<br>슬퍼도 기쁜 척 할 수가 있었어
+<br>널 위해서라면 난
+<br>아파도 강한 척 할 수가 있었어
+<br>사랑이 사랑만으로 완벽하길
+<br>내 모든 약점들은 다 숨겨지길
+<br>이뤄지지 않는 꿈속에서
+<br>피울 수 없는 꽃을 키웠어
+<br>
+<br>I''m so sick of this
+<br>Fake Love Fake Love Fake Love
+<br>I''m so sorry but it''s
+<br>Fake Love Fake Love Fake Love
+<br>
+<br>I wanna be a good man just for you
+<br>세상을 줬네 just for you
+<br>전부 바꿨어 just for you
+<br>Now I dunno me, who are you?
+<br>우리만의 숲 너는 없었어
+<br>내가 왔던 route 잊어버렸어
+<br>나도 내가 누구였는지도 잘 모르게 됐어
+<br>거울에다 지껄여봐 너는 대체 누구니
+<br>
+<br>널 위해서라면 난
+<br>슬퍼도 기쁜 척 할 수가 있었어
+<br>널 위해서라면 난
+<br>아파도 강한 척 할 수가 있었어
+<br>사랑이 사랑만으로 완벽하길
+<br>내 모든 약점들은 다 숨겨지길
+<br>이뤄지지 않는 꿈속에서
+<br>피울 수 없는 꽃을 키웠어
+<br>
+<br>Love you so bad Love you so bad 
+<br>널 위해 예쁜 거짓을 빚어내
+<br>Love it’s so mad Love it’s so mad 
+<br>날 지워 너의 인형이 되려 해
+<br>Love you so bad Love you so bad
+<br>널 위해 예쁜 거짓을 빚어내
+<br>Love it’s so mad Love it’s so mad
+<br>날 지워 너의 인형이 되려 해
+<br>
+<br>I''m so sick of this
+<br>Fake Love Fake Love Fake Love
+<br>I''m so sorry but it''s
+<br>Fake Love Fake Love Fake Love
+<br>
+<br>Why you sad? I don''t know 난 몰라
+<br>웃어봐 사랑해 말해봐
+<br>나를 봐 나조차도 버린 나
+<br>너조차 이해할 수 없는 나
+<br>낯설다 하네 니가 좋아하던 나로 변한 내가
+<br>아니라 하네 예전에 니가 잘 알고 있던 내가
+<br>아니긴 뭐가 아냐 난 눈 멀었어
+<br>사랑은 뭐가 사랑 It''s all fake love
+<br>
+<br>(Woo) I dunno I dunno I dunno why
+<br>(Woo) 나도 날 나도 날 모르겠어
+<br>(Woo) I just know I just know I just know why
+<br>Cuz it’s all Fake Love Fake Love Fake Love
+');
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (102, '아이유', '<br>이른 아침 작은 새들 노랫소리 들려오면 
+<br>언제나 그랬듯 아쉽게 잠을 깬다 
+<br>창문 하나 햇살 가득 눈부시게 비쳐오고 
+<br>서늘한 냉기에 재채기할까 말까
+<br>
+<br>눈 비비며 빼꼼히 창밖을 내다보니 
+<br>삼삼오오 아이들은 재잘대며 학교 가고 
+<br>산책 갔다 오시는 아버지의 양손에는 
+<br>효과를 알 수 없는 약수가 하나 가득
+<br> 
+<br>딸각딸각 아침 짓는 어머니의 분주함과 
+<br>엉금엉금 냉수 찾는 그 아들의 게으름이 
+<br>상큼하고 깨끗한 아침의 향기와 
+<br>구수하게 밥 뜸드는 냄새가 어우러진 
+<br>
+<br>가을 아침 내겐 정말 커다란 기쁨이야 
+<br>가을 아침 내겐 정말 커다란 행복이야 
+<br>응석만 부렸던 내겐 
+<br>
+<br>파란 하늘 바라보며 커다란 숨을 쉬니 
+<br>드높은 하늘처럼 내 마음 편해지네
+<br>텅 빈 하늘 언제 왔나 고추잠자리 하나가
+<br>잠 덜 깬 듯 엉성히 돌기만 비잉비잉
+<br>
+<br>토닥토닥 빨래하는 어머니의 분주함과
+<br>동기동기 기타 치는 그 아들의 한가함이
+<br>심심하면 쳐대는 괘종시계 종소리와
+<br>시끄러운 조카들의 울음소리 어우러진
+<br>
+<br>가을 아침 내겐 정말 커다란 기쁨이야
+<br>가을 아침 내겐 정말 커다란 행복이야
+<br>응석만 부렸던 내겐
+<br>
+<br>가을 아침 내겐 정말 커다란 기쁨이야
+<br>가을 아침 내겐 정말 커다란 행복이야
+<br>뜬구름 쫓았던 내겐
+<br>
+<br>이른 아침 작은 새들 노랫소리 들려오면
+<br>언제나 그랬듯 아쉽게 잠을 깬다
+<br>창문 하나 햇살 가득 눈부시게 비쳐오고
+<br>서늘한 냉기에 재채기할까 말까');
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (103, '아이유', '<br>모두 잠드는 밤에
+<br>혼자 우두커니 앉아
+<br>다 지나버린 오늘을
+<br>보내지 못하고서 깨어있어
+<br>
+<br>누굴 기다리나 
+<br>아직 할 일이 남아 있었던가
+<br>그것도 아니면 돌아가고 싶은
+<br>그리운 자리를 떠올리나
+<br>
+<br>무릎을 베고 누우면 
+<br>나 아주 어릴 적 그랬던 것처럼
+<br>머리칼을 넘겨줘요
+<br>
+<br>그 좋은 손길에 까무룩 잠이 들어도
+<br>잠시만 그대로 두어요
+<br>깨우지 말아요 아주
+<br>깊은 잠을 잘 거예요
+<br>
+<br>조용하던 두 눈을
+<br>다시 나에게 내리면
+<br>나 그때처럼 말갛게 웃어 보일 수 있을까
+<br>
+<br>나 지친 것 같아
+<br>이 정도면 오래 버틴 것 같아
+<br>그대 있는 곳에 돌아갈 수 있는
+<br>지름길이 있다면 좋겠어
+<br>
+<br>무릎을 베고 누우면 
+<br>나 아주 어릴 적 그랬던 것처럼
+<br>머리칼을 넘겨줘요
+<br>
+<br>그 좋은 손길에 까무룩 잠이 들어도
+<br>잠시만 그대로 두어요
+<br>깨우지 말아요 아주
+<br>깊은 잠을 잘 거예요
+<br>
+<br>스르르르륵 스르르
+<br>깊은 잠을 잘 거예요
+<br>스르르르륵 스르르
+<br>깊은 잠을…');
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (104, '방탄소년단', '<br>보고 싶다 이렇게 
+<br>말하니까 더 보고 싶다
+<br>너희 사진을 
+<br>보고 있어도 보고 싶다
+<br>
+<br>너무 야속한 시간 
+<br>나는 우리가 밉다
+<br>이제 얼굴 한 번 보는 것 조차
+<br>힘들어진 우리가
+<br>여긴 온통 겨울 뿐이야 
+<br>8월에도 겨울이 와
+<br>
+<br>마음은 시간을 달려가네 
+<br>홀로 남은 설국열차
+<br>니 손 잡고 지구 
+<br>반대편까지 가 
+<br>이 겨울을 끝내고파
+<br>
+<br>그리움들이 
+<br>얼마나 눈처럼 내려야 
+<br>그 봄날이 올까
+<br>Friend
+<br>
+<br>허공을 떠도는 
+<br>작은 먼지처럼 
+<br>작은 먼지처럼
+<br>날리는 눈이 나라면 
+<br>조금 더 빨리 네게
+<br>닿을 수 있을 텐데
+<br>
+<br>눈꽃이 떨어져요
+<br>또 조금씩 멀어져요
+<br>보고 싶다
+<br>보고 싶다
+<br>얼마나 기다려야 
+<br>또 몇 밤을 더 새워야
+<br>널 보게 될까 
+<br>만나게 될까
+<br>
+<br>추운 겨울 끝을 지나 
+<br>다시 봄날이 올 때까지
+<br>꽃 피울 때까지 
+<br>그곳에 좀 더 머물러줘 
+<br>머물러줘
+<br>
+<br>니가 변한 건지 
+<br>아니면 내가 변한 건지
+<br>이 순간 흐르는 시간조차 미워 
+<br>우리가 변한 거지 뭐 
+<br>모두가 그런 거지 뭐
+<br>
+<br>그래 밉다 니가 
+<br>넌 떠났지만 
+<br>단 하루도 너를 
+<br>잊은 적이 없었지 난
+<br>
+<br>솔직히 보고 싶은데 
+<br>이만 너를 지울게
+<br>그게 널 원망하기보단 
+<br>덜 아프니까
+<br>
+<br>시린 널 불어내 본다 
+<br>연기처럼 하얀 연기처럼
+<br>말로는 지운다 해도 
+<br>사실 난 아직 널 보내지 못하는데
+<br>
+<br>눈꽃이 떨어져요 
+<br>또 조금씩 멀어져요
+<br>보고 싶다 
+<br>보고 싶다
+<br>얼마나 기다려야 
+<br>또 몇 밤을 더 새워야
+<br>널 보게 될까 
+<br>만나게 될까
+');
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (105, '방탄소년단', '<br>불타오르네
+<br>
+<br>Fire
+<br>Fire
+<br>Fire
+<br>Fire
+<br>
+<br>When I wake up in my room 난 뭣도 없지
+<br>해가 지고 난 후 비틀대며 걷지
+<br>다 만신창이로 취했어 취했어
+<br>막 욕해 길에서 길에서
+<br>나 맛이 갔지 미친놈 같지
+<br>다 엉망진창, livin'' like 삐-이-
+<br>
+<br>니 멋대로 살어 어차피 니 꺼야
+<br>애쓰지 좀 말어 져도 괜찮아
+<br>Errbody say La la la la la (La la la la la)
+<br>Say La la la la la (La la la la la)
+<br>손을 들어 소리질러 Burn it up
+<br>불타오르네
+<br>
+<br>싹 다 불태워라 Bow wow wow
+<br>싹 다 불태워라 Bow wow wow 
+<br>
+<br>Hey, burn it up
+<br>전부 다 태울 것 같이
+<br>Hey, turn it up
+<br>새벽이 다 갈 때까지
+<br>그냥 살아도 돼 우린 젊기에
+<br>그 말하는 넌 뭔 수저길래
+<br>수저수저 거려 난 사람인데
+<br>(So what~)
+<br>
+<br>니 멋대로 살어 어차피 니 꺼야
+<br>애쓰지 좀 말어 져도 괜찮아
+<br>Errbody say La la la la la (La la la la la)
+<br>Say La la la la la (La la la la la)
+<br>손을 들어 소리질러 Burn it up
+<br>불타오르네
+<br>
+<br>싹 다 불태워라 Bow wow wow
+<br>싹 다 불태워라 Bow wow wow
+<br>
+<br>(Fire) 겁 많은 자여 여기로
+<br>(Fire) 괴로운 자여 여기로
+<br>(Fire) 맨주먹을 들고 All night long
+<br>(Fire) 진군하는 발걸음으로
+<br>(Fire) 뛰어봐 미쳐버려 다
+<br>
+<br>싹 다 불태워라 Bow wow wow
+<br>싹 다 불태워라 Bow wow wow
+<br>
+<br>Fire 
+<br>Fire 
+<br>싹 다 불태워라 Bow wow wow
+<br>
+<br>Fire 
+<br>Fire 
+<br>싹 다 불태워라 Bow wow wow
+<br>
+<br>용서해줄게');
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (106, '아이유', '<br>미리 말할게 사과는 안 해
+<br>아무 말 없이 너 후회 안 해?
+<br>다 관심 없잖아 친구야 뭐야
+<br>Oh you know what to do
+<br>
+<br>피곤해 그만 오늘은 놔 줘 
+<br>더 이상 반복하긴 싫어
+<br>또 다 내가 나빠 
+<br>아마 그래 난 
+<br>널 미워하나 봐 
+<br>
+<br>사랑이 잘 안 돼
+<br>떠올려 봐도
+<br>피부를 부비고 안아봐도 
+<br>입술을 맞춰도 참
+<br>생각대로 되지 않아
+<br>웃긴 것 같아
+<br>되돌려보려고 
+<br>서로 모른 척해도
+<br>이제 와 우리가 어떻게
+<br>다시 사랑 같은 걸 하겠어
+<br>
+<br>다섯 번째 미안하단 말이
+<br>이젠 너에게는 지겨운 건지
+<br>마지막일 거란 예감이 들어
+<br>Oh you know what to do
+<br>이건 내가 오늘 하루 종일
+<br>그린 저녁은 분명 아니야
+<br>널 보면 자꾸 네 안에 
+<br>내가 보여서
+<br>이젠 내가 싫어
+<br>
+<br>사랑이 잘 안 돼
+<br>떠올려 봐도
+<br>피부를 부비고 안아봐도 
+<br>입술을 맞춰도 참
+<br>생각대로 되지 않아
+<br>웃긴 것 같아
+<br>되돌려보려고 
+<br>계속 모른 척해도
+<br>이제 와 우리가 어떻게
+<br>다시 사랑 같은 걸 하겠어
+<br>
+<br>어디야 
+<br>넌 어디야 
+<br>집이야 
+<br>난 택시야 
+<br>집에 거의 다 와가니
+<br>
+<br>Oh 미안해 
+<br>뭐 어떤 게 
+<br>그냥 다
+<br>들어가
+<br>나 지갑 거기 두고 왔어
+<br>
+<br>Oh 있잖아 
+<br>아니야 
+<br>말해 봐 
+<br>이제 더 
+<br>사랑하지 않는 것 같아
+<br>아
+<br>
+<br>사랑이 잘 안 돼
+<br>떠올려 봐도
+<br>피부를 부비고 안아봐도 
+<br>입술을 맞춰도 참
+<br>생각대로 되지 않아
+<br>웃긴 것 같아
+<br>되돌려보려고 
+<br>서로 모른 척해도
+<br>이제 와 우리가 어떻게
+<br>다시 사랑 같은 걸 하겠어');
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (107, '방탄소년단', '<br>내 피 땀 눈물 내 마지막 춤을 
+<br>다 가져가 가
+<br>내 피 땀 눈물 내 차가운 숨을 
+<br>다 가져가 가
+<br>내 피 땀 눈물
+<br>
+<br>내 피 땀 눈물도 
+<br>내 몸 마음 영혼도
+<br>너의 것인 걸 잘 알고 있어
+<br>이건 나를 벌받게 할 주문
+<br>Peaches and cream
+<br>Sweeter than sweet
+<br>Chocolate cheeks
+<br>and chocolate wings
+<br>But 너의 날개는 악마의 것
+<br>너의 그 sweet 앞엔 bitter bitter
+<br>Kiss me 아파도 돼 어서 날 조여줘
+<br>더 이상 아플 수도 없게
+<br>Baby 취해도 돼 이제 널 들이켜
+<br>목 깊숙이 너란 위스키
+<br>
+<br>내 피 땀 눈물 내 마지막 춤을 
+<br>다 가져가 가
+<br>내 피 땀 눈물 내 차가운 숨을 
+<br>다 가져가 가
+<br>
+<br>원해 많이 많이 많이 많이
+<br>원해 많이 많이 많이 많이 많이 많이
+<br>원해 많이 많이 많이 많이
+<br>원해 많이 많이 많이 많이 많이 많이
+<br>
+<br>아파도 돼 날 묶어줘 내가 도망칠 수 없게
+<br>꽉 쥐고 날 흔들어줘 내가 정신 못 차리게
+<br>Kiss me on the lips lips 둘만의 비밀
+<br>너란 감옥에 중독돼 깊이
+<br>니가 아닌 다른 사람 섬기지 못해
+<br>알면서도 삼켜버린 독이 든 성배
+<br>
+<br>내 피 땀 눈물 내 마지막 춤을 
+<br>다 가져가 가
+<br>내 피 땀 눈물 내 차가운 숨을 
+<br>다 가져가 가
+<br>
+<br>원해 많이 많이 많이 많이
+<br>원해 많이 많이 많이 많이 많이 많이
+<br>원해 많이 많이 많이 많이
+<br>원해 많이 많이 많이 많이 많이 많이
+<br>
+<br>나를 부드럽게 죽여줘
+<br>너의 손길로 눈 감겨줘
+<br>어차피 거부할 수조차 없어
+<br>더는 도망갈 수조차 없어
+<br>니가 너무 달콤해 너무 달콤해
+<br>너무 달콤해서
+<br>
+<br>내 피 땀 눈물
+<br>내 피 땀 눈물');
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (9, ' 아이유', '<br>So are you happy now
+<br>Finally happy now are you
+<br>
+<br>뭐 그대로야 난 
+<br>다 잃어버린 것 같아  
+<br>
+<br>모든 게 맘대로 왔다가 인사도 없이 떠나
+<br>이대로는 무엇도 사랑하고 싶지 않아 
+<br>다 해질 대로 해져버린
+<br>기억 속을 여행해 
+<br>
+<br>우리는 오렌지 태양 아래
+<br>그림자 없이 함께 춤을 춰
+<br>정해진 이별 따위는 없어 
+<br>아름다웠던 그 기억에서 만나 
+<br>Forever young 
+<br>
+<br>우우우 우우우우  우우우 우우우우
+<br>Forever we young 
+<br>우우우 우우우우
+<br>이런 악몽이라면 영영 깨지 않을게 
+<br>
+<br>섬 그래 여긴 섬 서로가 만든 작은 섬
+<br>예 음 forever young 영원이란 말은 모래성
+<br>
+<br>작별은 마치 재난문자 같지
+<br>그리움과 같이 맞이하는 아침
+<br>서로가 이 영겁을 지나 
+<br>꼭 이 섬에서 다시 만나
+<br>
+<br>지나듯 날 위로하던 누구의 말대로 고작
+<br>한 뼘짜리 추억을 잊는 게 참 쉽지 않아
+<br>시간이 지나도 여전히
+<br>날 붙드는 그곳에 
+<br>
+<br>우리는 오렌지 태양 아래
+<br>그림자 없이 함께 춤을 춰
+<br>정해진 안녕 따위는 없어 
+<br>아름다웠던 그 기억에서 만나 
+<br>
+<br>우리는 서로를 베고 누워
+<br>슬프지 않은 이야기를 나눠
+<br>우울한 결말 따위는 없어
+<br>난 영원히 널 이 기억에서 만나 
+<br>Forever young 
+<br>
+<br>우우우 우우우우  우우우 우우우우
+<br>Forever we young
+<br>우우우 우우우우
+<br>이런 악몽이라면 영영 깨지 않을게');
+INSERT INTO MVP.AUDIO (NO, SINGER, CONTENT) VALUES (17, ' 아이유', '<br>뭐해?라는 두 글자에
+<br>네가 보고 싶어 나의 속마음을 담아 우
+<br>이모티콘 하나하나 속에
+<br>달라지는 내 미묘한 심리를 알까 우
+<br>
+<br>아니 바쁘지 않아 nothing no no 
+<br>잠들어 있지 않아 insomnia nia nia
+<br>지금 다른 사람과 함께이지 않아
+<br>응, 나도 너를 생각 중
+<br>
+<br>우리의 네모 칸은 bloom
+<br>엄지손가락으로 장미꽃을 피워 
+<br>향기에 취할 것 같아 우
+<br>오직 둘만의 비밀의 정원
+<br>
+<br>I feel bloom I feel bloom I feel bloom
+<br>너에게 한 송이를 더 보내
+<br>
+<br>밤샘 작업으로 업데이트
+<br>흥미로운 이 작품의 지은이 that’s me 우
+<br>어쩜 이 관계의 클라이맥스
+<br>2막으로 넘어가기엔 지금이 good timing 우
+<br>
+<br>같은 맘인 걸 알아 realize la lize
+<br>말을 고르지 말아 just reply la la ly
+<br>조금 장난스러운 나의 은유에
+<br>네 해석이 궁금해 
+<br>
+<br>우리의 색은 gray and blue 
+<br>엄지손가락으로 말풍선을 띄워
+<br>금세 터질 것 같아 우
+<br>호흡이 가빠져 어지러워
+<br>
+<br>I feel blue. I feel blue. I feel blue.
+<br>너에게 가득히 채워 
+<br>
+<br>띄어쓰기없이보낼게사랑인것같애
+<br>백만송이장미꽃을, 나랑피워볼래?
+<br>꽃잎의 색은 우리 마음 가는 대로 칠해
+<br>시들 때도 예쁘게
+<br>
+<br>우리의 네모 칸은 bloom
+<br>엄지손가락으로 장미꽃을 피워 
+<br>향기에 취할 것 같아 우
+<br>오직 둘만의 비밀의 정원
+<br>
+<br>I feel bloom I feel bloom I feel bloom
+<br>너에게 한 송이를 더 보내
+');
